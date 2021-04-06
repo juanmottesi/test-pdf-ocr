154de3b9d89f9d84143e51ac29d4b2acf5c4626f
jmottesi <jmottesi@portfoliopersonal.com>
2021-04-06 11:39:38 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,91 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useRef, useState } from 'react';
+import Webcam from "react-webcam";
+import { BrowserPDF417Reader } from '@zxing/browser';
 
-export default function Home() {
+const DniFront = ({ setStep, setData }) => {
+  const [loading, setLoading] = useState(true);
+  const [videoConstraints, setVideoConstraints] = useState({ width: 360, height: 640 });
+  const webcamRef = useRef(null);
+  useEffect(() => {
+    setVideoConstraints({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    setLoading(false);
+  }, [])
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setData(imageSrc);
+    setStep('showDniFront');
+  }
+
+  return !loading && <>
+    <Webcam ref={webcamRef} audio={false}  screenshotFormat="image/png" videoConstraints={videoConstraints} />
+    <button onClick={capture}>Foto</button>
+    </>
+}
+
+const DniFrontInfo = ({ data, setStep }) => {
+  const [pdf417, setPdf417] = useState('');
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const codeReader = new BrowserPDF417Reader();
+    const sourceElem = document.querySelector('#image');
+    console.log('????', sourceElem);
+    codeReader.decodeFromImageElement(sourceElem).then((r) => { setPdf417(r)}).catch(() => setError('T_T'))
+  },[]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <img id="image" src={data} width="60%" height="30%" />
+      { pdf417 && 
+        <>
+          <div>{pdf417.text}</div>
+          <button onClick={() => setStep('askDniBack')}>next</button>
+        </>
+      }
+      { error && <button onClick={() => setStep('askDniFront')}>Error!!! volver</button>}
     </div>
   )
 }
+
+
+const Intro = ({ setStep }) => (
+  <div>
+    <div>
+      <h1>Toma una foto del frente de tu DNI</h1>
+      <p>Buscá un lugar con buena iluminación</p>
+      <p>Asegurate que se lean bien los datos</p>
+      <p>Evita los reflejos y las sombras</p>
+      <p>Apoya el DNI sobre fondo liso</p>
+    </div>
+    <button className="btn btn-primary" onClick={() => setStep('askDniFront')}>Saca la foto</button>
+  </div>
+);
+
+const Home = () => {
+  const [step, setStep] = useState('start')
+
+  const [dniFront, setDniFront] = useState(null);
+  const [dniBack, setDniBack] = useState(null);
+
+  const renderElement = () => {
+    switch (step) {
+      case 'start': return <Intro setStep={setStep} />;
+      case 'askDniFront': return <DniFront setData={setDniFront} setStep={setStep} />;
+      case 'showDniFront': return <DniFrontInfo data={dniFront} setStep={setStep} />;
+      case 'askDniBack': return <DniBack setData={setDniBack} setStep={setStep} />;
+      case 'showDniBack': return <DniBackInfo data={dniBack} setStep={setStep} />;
+      default: <div>????</div>;
+    }
+  }
+
+  return (
+    <div>
+      {renderElement()}
+    </div>
+  )
+}
+
+export default Home;
