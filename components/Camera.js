75245e00e931fbaba5b5eca2ef67cd64647fad1e
jmottesi <jmottesi@portfoliopersonal.com>
2021-04-07 11:29:17 -0300

    add switch camera

--- components/Camera.js
+++ components/Camera.js
@@ -1,9 +1,10 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Camera as CameraPro } from "react-camera-pro";
 import styles from '../styles/Home.module.css';
 
 const Camera = ({ setData, setStep, nextStep }) => {
   const cameraRef = useRef(null);
+  const [numberOfCameras, setNumberOfCameras] = useState(0);
 
   const handleClick = () => {
     setData(cameraRef.current.takePhoto());
@@ -11,10 +12,11 @@ const Camera = ({ setData, setStep, nextStep }) => {
   }
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} numberOfCamerasCallback={setNumberOfCameras}>
       <CameraPro ref={cameraRef} facingMode="environment" />
       <div className={styles.border} />
       <button className={styles.button} onClick={handleClick}>Take photo</button>
+      <button hidden={numberOfCameras <= 1} className={styles.button2} onClick={() => { camera.current.switchCamera(); }} />
     </div>
   );
 }
